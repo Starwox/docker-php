ded5c933085d97d2102585d7eef90bad5d085c4a
UgoRicher <ugo.richer77@hotmail.fr>
2021-12-04 19:31:13 +0100

    routes v1 routes v1
    merci
    c

--- app/index.php
+++ app/index.php
@@ -1,4 +1,15 @@
 <?php
+$request = $_SERVER['REQUEST_URI'];
+
+switch ($request) {
+    case '/' :
+        require __DIR__ . '/views/index.php';
+        break;
+    case '' :
+        require __DIR__ . '/views/index.php';
+        break;
+}
+
 ?>
 
 <html>
